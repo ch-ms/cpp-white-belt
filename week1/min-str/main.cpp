e382b494cd9d9ae3003432ecc66c2b754673ba7e
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2017-12-01 01:05:12 +0300

    ref

--- week1/min-str/main.cpp
+++ week1/min-str/main.cpp
@@ -6,18 +6,13 @@ int main() {
   string x, y, z;
   cin >> x >> y >> z;
 
-  if (x < y) {
-    if (x < z) {
-      cout << x;
-    } else {
-      cout << z;
-    }
+  if (x < y && x < z) {
+    cout << x;
+  } else if (y < z) {
+    cout << y;
   } else {
-    if (y < z) {
-      cout << y;
-    } else {
-      cout << z;
-    }
+    cout << z;
   }
+
   return 0;
 }
