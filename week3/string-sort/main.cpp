ba20cad92b3e787d62954bdee2264a4aa9bb245b
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2017-12-25 23:55:00 +0300

    Sort to lower

---
+++ week3/string-sort/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string StringToLower(string s) {
+  for(auto& c : s) {
+    c = tolower(c);
+  }
+  return s;
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<string> vec;
+  while(n > 0) {
+    n -= 1;
+    string s;
+    cin >> s;
+    vec.push_back(s);
+  }
+
+  sort(begin(vec), end(vec), [](const string& s1, const string& s2) {
+    return StringToLower(s1) < StringToLower(s2);});
+
+  for(const auto& s : vec) {
+    cout << s << " ";
+  }
+  return 0;
+}
