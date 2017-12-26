7ae27fdfec43ee9a8618683bac7bd12148d5ee86
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2017-12-27 02:42:11 +0300

    Incognizable

---
+++ week3/incognizable/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Incognizable.cpp"
+using namespace std;
+
+int main() {
+  Incognizable a;
+  Incognizable b = {};
+  Incognizable c = {0};
+  Incognizable d = {0, 1};
+  return 0;
+}
